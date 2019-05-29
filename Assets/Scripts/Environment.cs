08548243d1165c0cf57548f7b98e224778ad776b
Paweł Matusek <pmatusek1@wp.pl>
2019-05-29 17:18:47 +0200

    player spawner

--- Assets/Scripts/Environment.cs
+++ Assets/Scripts/Environment.cs
@@ -6,39 +6,43 @@ public class Environment : MonoBehaviour
 
     public GameObject AirTilePrefab;
 
-    public GameObject PlayerPrefab;
+    public GameObject PlayerSpawnerPrefab;
 
     private const float TileUnit = 0.64f;
 
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(this.SolidTilePrefab, new Vector3(-1.5f * TileUnit, -2f * TileUnit, 0), Quaternion.identity).transform.parent = this.gameObject.transform;
-        Instantiate(this.SolidTilePrefab, new Vector3(-0.5f * TileUnit, -2f * TileUnit, 0), Quaternion.identity).transform.parent = this.gameObject.transform;
-        Instantiate(this.SolidTilePrefab, new Vector3(0.5f * TileUnit, -2f * TileUnit, 0), Quaternion.identity).transform.parent = this.gameObject.transform;
-        Instantiate(this.SolidTilePrefab, new Vector3(1.5f * TileUnit, -2f * TileUnit, 0), Quaternion.identity).transform.parent = this.gameObject.transform;
+        Instantiate(this.SolidTilePrefab, new Vector3(-1.5f * TileUnit, -2.0f * TileUnit, 0), Quaternion.identity).transform.parent = this.gameObject.transform;
+        Instantiate(this.SolidTilePrefab, new Vector3(-0.5f * TileUnit, -2.0f * TileUnit, 0), Quaternion.identity).transform.parent = this.gameObject.transform;
+        Instantiate(this.SolidTilePrefab, new Vector3(0.5f * TileUnit, -2.0f * TileUnit, 0), Quaternion.identity).transform.parent = this.gameObject.transform;
+        Instantiate(this.SolidTilePrefab, new Vector3(1.5f * TileUnit, -2.0f * TileUnit, 0), Quaternion.identity).transform.parent = this.gameObject.transform;
         //
-        Instantiate(this.SolidTilePrefab, new Vector3(-1.5f * TileUnit, -1f * TileUnit, 0), Quaternion.identity).transform.parent = this.gameObject.transform;
-        Instantiate(this.AirTilePrefab, new Vector3(-0.5f * TileUnit, -1f * TileUnit, 0), Quaternion.identity).transform.parent = this.gameObject.transform;
-        Instantiate(this.AirTilePrefab, new Vector3(0.5f * TileUnit, -1f * TileUnit, 0), Quaternion.identity).transform.parent = this.gameObject.transform;
-        Instantiate(this.SolidTilePrefab, new Vector3(1.5f * TileUnit, -1f * TileUnit, 0), Quaternion.identity).transform.parent = this.gameObject.transform;
+        Instantiate(this.SolidTilePrefab, new Vector3(-1.5f * TileUnit, -1.0f * TileUnit, 0), Quaternion.identity).transform.parent = this.gameObject.transform;
+        Instantiate(this.AirTilePrefab, new Vector3(-0.5f * TileUnit, -1.0f * TileUnit, 0), Quaternion.identity).transform.parent = this.gameObject.transform;
+        Instantiate(this.AirTilePrefab, new Vector3(0.5f * TileUnit, -1.0f * TileUnit, 0), Quaternion.identity).transform.parent = this.gameObject.transform;
+        Instantiate(this.SolidTilePrefab, new Vector3(1.5f * TileUnit, -1.0f * TileUnit, 0), Quaternion.identity).transform.parent = this.gameObject.transform;
         //
-        Instantiate(this.SolidTilePrefab, new Vector3(-1.5f * TileUnit, 0 * TileUnit, 0), Quaternion.identity).transform.parent = this.gameObject.transform;
-        Instantiate(this.AirTilePrefab, new Vector3(-0.5f * TileUnit, 0 * TileUnit, 0), Quaternion.identity).transform.parent = this.gameObject.transform;
-        Instantiate(this.AirTilePrefab, new Vector3(0.5f * TileUnit, 0 * TileUnit, 0), Quaternion.identity).transform.parent = this.gameObject.transform;
-        Instantiate(this.SolidTilePrefab, new Vector3(1.5f * TileUnit, 0 * TileUnit, 0), Quaternion.identity).transform.parent = this.gameObject.transform;
+        Instantiate(this.SolidTilePrefab, new Vector3(-1.5f * TileUnit, 0.0f * TileUnit, 0), Quaternion.identity).transform.parent = this.gameObject.transform;
+        Instantiate(this.AirTilePrefab, new Vector3(-0.5f * TileUnit, 0.0f * TileUnit, 0), Quaternion.identity).transform.parent = this.gameObject.transform;
+        Instantiate(this.AirTilePrefab, new Vector3(0.5f * TileUnit, 0.0f * TileUnit, 0), Quaternion.identity).transform.parent = this.gameObject.transform;
+        Instantiate(this.SolidTilePrefab, new Vector3(1.5f * TileUnit, 0.0f * TileUnit, 0), Quaternion.identity).transform.parent = this.gameObject.transform;
         //
-        Instantiate(this.SolidTilePrefab, new Vector3(-1.5f * TileUnit, 1f * TileUnit, 0), Quaternion.identity).transform.parent = this.gameObject.transform;
-        Instantiate(this.AirTilePrefab, new Vector3(-0.5f * TileUnit, 1f * TileUnit, 0), Quaternion.identity).transform.parent = this.gameObject.transform;
-        Instantiate(this.AirTilePrefab, new Vector3(0.5f * TileUnit, 1f * TileUnit, 0), Quaternion.identity).transform.parent = this.gameObject.transform;
-        Instantiate(this.SolidTilePrefab, new Vector3(1.5f * TileUnit, 1f * TileUnit, 0), Quaternion.identity).transform.parent = this.gameObject.transform;
+        Instantiate(this.SolidTilePrefab, new Vector3(-1.5f * TileUnit, 1.0f * TileUnit, 0), Quaternion.identity).transform.parent = this.gameObject.transform;
+        Instantiate(this.AirTilePrefab, new Vector3(-0.5f * TileUnit, 1.0f * TileUnit, 0), Quaternion.identity).transform.parent = this.gameObject.transform;
+        Instantiate(this.AirTilePrefab, new Vector3(0.5f * TileUnit, 1.0f * TileUnit, 0), Quaternion.identity).transform.parent = this.gameObject.transform;
+        Instantiate(this.SolidTilePrefab, new Vector3(1.5f * TileUnit, 1.0f * TileUnit, 0), Quaternion.identity).transform.parent = this.gameObject.transform;
         //
-        Instantiate(this.SolidTilePrefab, new Vector3(-1.5f * TileUnit, 2f * TileUnit, 0), Quaternion.identity).transform.parent = this.gameObject.transform;
-        Instantiate(this.SolidTilePrefab, new Vector3(-0.5f * TileUnit, 2f * TileUnit, 0), Quaternion.identity).transform.parent = this.gameObject.transform;
-        Instantiate(this.SolidTilePrefab, new Vector3(0.5f * TileUnit, 2f * TileUnit, 0), Quaternion.identity).transform.parent = this.gameObject.transform;
-        Instantiate(this.SolidTilePrefab, new Vector3(1.5f * TileUnit, 2f * TileUnit, 0), Quaternion.identity).transform.parent = this.gameObject.transform;
+        Instantiate(this.SolidTilePrefab, new Vector3(-1.5f * TileUnit, 2.0f * TileUnit, 0), Quaternion.identity).transform.parent = this.gameObject.transform;
+        Instantiate(this.SolidTilePrefab, new Vector3(-0.5f * TileUnit, 2.0f * TileUnit, 0), Quaternion.identity).transform.parent = this.gameObject.transform;
+        Instantiate(this.SolidTilePrefab, new Vector3(0.5f * TileUnit, 2.0f * TileUnit, 0), Quaternion.identity).transform.parent = this.gameObject.transform;
+        Instantiate(this.SolidTilePrefab, new Vector3(1.5f * TileUnit, 2.0f * TileUnit, 0), Quaternion.identity).transform.parent = this.gameObject.transform;
         //
-        Instantiate(this.PlayerPrefab, Vector3.zero, Quaternion.identity);
+        var playerSpawnerGameObject = Instantiate(this.PlayerSpawnerPrefab, new Vector3(0.0f * TileUnit, 0.0f * TileUnit, 0), Quaternion.identity);
+        playerSpawnerGameObject.transform.parent = this.gameObject.transform;
+        var playerSpawner = playerSpawnerGameObject.GetComponent<PlayerSpawner>();
+        playerSpawner.Id = string.Empty;
+        playerSpawner.SpawnPlayer();
     }
 
     // Update is called once per frame
---
+++ Assets/Scripts/PlayerSpawner.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSpawner : MonoBehaviour
+{
+    public const int TileWidth = 2;
+
+    public const int TileHeight = 3;
+
+    private bool createPlayer;
+
+    private GameObject player;
+
+    public GameObject PlayerPrefab;
+
+    public string Id { get; set; }
+
+    public void SpawnPlayer()
+    {
+        this.createPlayer = true;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.player == null && this.createPlayer)
+        {
+            this.player = Instantiate(this.PlayerPrefab, this.gameObject.transform.position, Quaternion.identity);
+        }
+
+        this.createPlayer = false;
+    }
+}
