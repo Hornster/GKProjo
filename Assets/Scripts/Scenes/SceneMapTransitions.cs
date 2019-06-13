5b4fba43320928384dbd733de37895f734c6f00c
Karol Kozuch <karol199714@gmail.com>
2019-06-13 18:46:31 +0200

    Added menu and transients between scenes

---
+++ Assets/Scripts/Scenes/SceneMapTransitions.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneMapTransitions : MonoBehaviour
+{
+    [SerializeField]
+    private Animator _transitionAnimator;
+    [SerializeField]
+    private string _returnKeyName;
+    [SerializeField]
+    private string _menuSceneName;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown(_returnKeyName))
+        {
+            SceneManager.LoadScene(_menuSceneName);
+        }
+    }
+}
