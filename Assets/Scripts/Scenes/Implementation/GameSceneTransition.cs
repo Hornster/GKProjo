ae6b3c8e09fcd858dcb794d9ac135823960469e6
Karol Kozuch <karol199714@gmail.com>
2019-08-19 22:48:53 +0200

    Added finish screen

---
+++ Assets/Scripts/Scenes/Implementation/GameSceneTransition.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Assets.Scripts.Scenes.Contracts;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Assets.Scripts.Scenes.Implementation
+{
+    public class GameSceneTransition : MonoBehaviour, ISceneTransition
+    {
+        [SerializeField]
+        private float _endTransitionWaitTime;
+        [SerializeField]
+        private Animator _transitionAnimator;
+        [SerializeField]
+        private string _startSceneName;
+
+        private IEnumerator ChangeSceneToPlayLevel()
+        {
+            _transitionAnimator.SetTrigger("end");
+            yield return new WaitForSeconds(_endTransitionWaitTime);
+            SceneManager.LoadScene(_startSceneName);
+        }
+
+        public void OnTrigger()
+        {
+            StartCoroutine(ChangeSceneToPlayLevel());  //return value does not have to be used
+        }
+    }
+}
---
+++ Assets/Scripts/Scenes/Implementation/LeaveSceneTransition.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Assets.Scripts.Scenes.Contracts;
+using UnityEngine;
+
+namespace Assets.Scripts.Scenes.Implementation
+{
+    public class LeaveSceneTransition : MonoBehaviour, ISceneTransition
+    {
+        [SerializeField]
+        private float _endTransitionWaitTime;
+        [SerializeField]
+        private Animator _transitionAnimator;
+        [SerializeField]
+        private string _startSceneName;
+
+        public void OnTrigger()
+        {
+            StartCoroutine(LeaveGame());  //return value does not have to be used
+        }
+
+        /// <summary>
+        /// Called as callback when the user presses the Leave button in Main Menu
+        /// </summary>
+        private IEnumerator LeaveGame()
+        {
+            _transitionAnimator.SetTrigger("end");
+            yield return new WaitForSeconds(_endTransitionWaitTime);
+            Application.Quit();
+        }
+    }
+}
